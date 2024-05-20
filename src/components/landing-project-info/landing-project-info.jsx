01626278a9205e4e704f83746cee28e7ab2a24f2
NikitaBatryncha <batrynchan@gmail.com>
2024-05-20 20:53:02 +0300

    horizontal

--- src/components/landing-project-info/landing-project-info.jsx
+++ src/components/landing-project-info/landing-project-info.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import styles from './styles.module.scss';
 
-export default function LandingProjectInfo({ text }) { // Destructure text from props
+export default function LandingProjectInfo({ text }) {
   return (
     <div className={styles["info"]}>
-      {text}
+      {text.map((item, index) => (
+        <p key={index}>
+          <span>
+            <b>{item.title}</b>
+          </span>
+          :
+          {Array.isArray(item.item) ? (
+            <ul className={styles["list"]}>
+              {item.item.map((info, idx) => (
+                <li className={styles["list-item"]}>
+                  <span key={idx}>
+                     {info}
+                  </span>
+                </li>
+            ))}
+            </ul>
+
+          ) : (
+            <span> {item.item}</span>
+          )}
+        </p>
+      ))}
     </div>
   );
 }
--- src/components/landing-project-section/landing-project-section.jsx
+++ src/components/landing-project-section/landing-project-section.jsx
@@ -5,9 +5,173 @@ import styles from "./styles.module.scss";
 import LandingProjectTitle from "../landing-project-title/landing-project-title";
 import LandingProjectSwiper from "../landing-project-swiper/landing-project-swiper";
 import LandingProjectVideo from "../landing-project-video/landing-project-video";
-import LandingProjectInfo from "../landing-project-info/landing-project-info"
+import LandingProjectInfo from "../landing-project-info/landing-project-info";
+import LandingProjectHorizontalSwiper from "../landing-project-horizontalSwiper/landing-project-horizontalSwiper"
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const ProjectsData = [
+  {
+    title: "Клубный дом CVET 32",
+    subtitle: "Cvet-32 | 1",
+    slides: [
+      { id: 1, imageUrl: "images/slider/1.jpeg" },
+      { id: 2, imageUrl: "images/slider/2.jpeg" },
+      { id: 3, imageUrl: "images/slider/3.jpeg" },
+      { id: 4, imageUrl: "images/slider/4.jpeg" },
+      { id: 5, imageUrl: "images/slider/5.jpeg" },
+      { id: 6, imageUrl: "images/slider/6.jpeg" }
+    ],
+    info: [
+      {title: "ОБЬЕКТ", item: "Апартамент" },
+      {title: "РАСПОЛОЖЕНИЕ", item: "Цветной бул., 32А, Москва" },
+      {title: "ПЛОЩАДЬ", item: "86 М2" },
+      {title: "СРОК РЕАЛИЗАЦИИ", item: "2021 г" },
+      {title: "ЗАКАЗЧИК", item: "Апартамент" },
+      {
+        title: "Состав работ",
+        item: [
+          "комплекс отделочных работ включая проектирование и монтаж инженерных систем",
+          "управление проектом"
+        ]
+      },
+    ],
+    video: "/images/videos/qsk1.3gp",
+    secondSlider: null
+  },
+  {
+    title: null,
+    subtitle: "Cvet-32 | 2",
+    slides: [
+      { id: 1, imageUrl: "images/slider1/1.jpeg" },
+      { id: 2, imageUrl: "images/slider1/2.jpeg" },
+    ],
+    info: [
+      {title: "ОБЬЕКТ", item: "Апартамент" },
+      {title: "РАСПОЛОЖЕНИЕ", item: "Цветной бул., 32А, Москва" },
+      {title: "ПЛОЩАДЬ", item: "74 М2" },
+      {title: "СРОК РЕАЛИЗАЦИИ", item: "2021 г" },
+      {title: "ЗАКАЗЧИК", item: "Частное лицо" },
+      {title: "Состав работ", item: [
+        "комплекс отделочных работ включая проектирование и монтаж инженерных систем",
+        "управление проектом"
+        ]
+      },
+    ],
+    video: "",
+    secondSlider: [
+      { id: 1, imageUrl: "images/slider1/1.1.jpeg" },
+      { id: 2, imageUrl: "images/slider1/2.1.jpeg" },
+    ]
+  },
+  {
+    title: "Клубный дом MITTE",
+    subtitle: "MITTE ERDE",
+    slides: [
+      { id: 1, imageUrl: "images/slider2/1.jpeg" },
+      { id: 2, imageUrl: "images/slider2/2.jpeg" },
+    ],
+    info: [
+      {title: "ОБЬЕКТ", item: "Пилотный апартамент" },
+      {title: "РАСПОЛОЖЕНИЕ", item: "Цветной бул., 32А, Москва" },
+      {title: "ПЛОЩАДЬ", item: "63 М2" },
+      {title: "СРОК РЕАЛИЗАЦИИ", item: "2019 г" },
+      {title: "ЗАКАЗЧИК", item: "Hutton Development" },
+      {title: "Состав работ", item: [
+        "комплекс отделочных работ включая проектирование и монтаж инженерных систем",
+        "управление проектом"
+        ]
+      },
+    ],
+    video: null,
+    secondSlider: [
+      { id: 1, imageUrl: "images/slider2/1.1.jpeg" },
+      { id: 2, imageUrl: "images/slider2/2.1.jpeg" },
+    ]
+  },
+  {
+    title: "Клубный дом MITTE",
+    subtitle: "MITTE MOND",
+    slides: [
+      { id: 1, imageUrl: "images/slider3/1.jpeg" },
+      { id: 2, imageUrl: "images/slider3/2.jpeg" },
+    ],
+    info: [
+      {title: "ОБЬЕКТ", item: "Пилотный апартамент" },
+      {title: "РАСПОЛОЖЕНИЕ", item: "Цветной бул., 32А, Москва" },
+      {title: "ПЛОЩАДЬ", item: "63 М2" },
+      {title: "СРОК РЕАЛИЗАЦИИ", item: "2019 г" },
+      {title: "ЗАКАЗЧИК", item: "Hutton Development" },
+      {title: "Состав работ", item: [
+        "комплекс отделочных работ включая проектирование и монтаж инженерных систем",
+        "управление проектом"
+        ]
+      },
+    ],
+    video: null,
+    secondSlider: [
+      { id: 1, imageUrl: "images/slider3/1.1.jpeg" },
+      { id: 2, imageUrl: "images/slider3/2.1.jpeg" },
+    ]
+  },
+  {
+    title: "Клубный поселок ARTECO",
+    subtitle: "ArtEco",
+    slides: [
+      { id: 1, imageUrl: "images/slider4/1.jpeg" },
+      { id: 2, imageUrl: "images/slider4/2.jpeg" },
+      { id: 2, imageUrl: "images/slider4/3.jpeg" },
+
+    ],
+    info: [
+      {title: "ОБЬЕКТ", item: "Жилой дом" },
+      {title: "РАСПОЛОЖЕНИЕ", item: "Москва, к.п. ArtEco" },
+      {title: "ПЛОЩАДЬ", item: "413 М2" },
+      {title: "СРОК РЕАЛИЗАЦИИ", item: "в процессе" },
+      {title: "ЗАКАЗЧИК", item: "Hutton Development" },
+      {title: "Состав работ", item: [
+        "Строительно-отделочные",
+        "Инженерные",
+        "Техническое сопровождение"
+        ]
+      },
+    ],
+    video: null,
+    secondSlider: [
+      { id: 1, imageUrl: "images/slider4/1.1.jpeg" },
+      { id: 2, imageUrl: "images/slider4/2.1.jpeg" },
+      { id: 1, imageUrl: "images/slider4/3.1.jpeg" },
+      { id: 2, imageUrl: "images/slider4/4.1.jpeg" },
+    ]
+  },
+  {
+    title: "Центр международной торговли",
+    subtitle: "ЦМТ",
+    slides: [
+      { id: 1, imageUrl: "images/slider5/1.jpeg" },
+      { id: 2, imageUrl: "images/slider5/2.jpeg" },
+      { id: 2, imageUrl: "images/slider5/3.jpeg" },
+      { id: 2, imageUrl: "images/slider5/4.jpeg" },
+      { id: 2, imageUrl: "images/slider5/5.jpeg" },
+      { id: 2, imageUrl: "images/slider5/6.jpeg" },
+      { id: 2, imageUrl: "images/slider5/7.jpeg" },
+    ],
+    info: [
+      {title: "ОБЬЕКТ", item: "Офис" },
+      {title: "РАСПОЛОЖЕНИЕ", item: "Краснопресненская набережная, дом 12, Москва" },
+      {title: "ПЛОЩАДЬ", item: "400 М2" },
+      {title: "СРОК РЕАЛИЗАЦИИ", item: "2021 г." },
+      {title: "ЗАКАЗЧИК", item: "АО «Сенс»" },
+      {title: "Состав работ", item: [
+        "комплекс отделочных работ включая проектирование и монтаж инженерных систем",
+        "управление проектом",
+        ]
+      },
+    ],
+    video: null,
+    secondSlider: null
+  }
+]
 
 export default function LandingProjectSection() {
   const container = useRef(null);
@@ -42,14 +206,17 @@ export default function LandingProjectSection() {
     <div className={styles.pageContainer}>
       <section ref={container} className={styles.section}>
         <div className={styles["section-wrapper"]}>
-          <div className={styles["section-horizontal"]}>
-            <LandingProjectTitle title={"Клубный дом Mitte"} />
-            <LandingProjectSwiper />
-            <LandingProjectInfo text={["ОБЬЕКТ: Апартамент","РАСПОЛОЖЕНИЕ: Цветной бул., 32А, Москва","ПЛОЩАДЬ: 86 М2","СРОК РЕАЛИЗАЦИИ: 2021 г","ЗАКАЗЧИК: Частное лицо","Состав работ:","комплекс отделочных работ включая проектирование и монтаж инженерных систем управление проектом"]}/>
-          </div>
-          <LandingProjectVideo video={"/images/videos/qsk1.3gp"} />
-          <button ref={button} onClick={scrollToTop} className={styles.button}></button>
-        </div>
+          {ProjectsData.map((project, index) => (
+            <div className={styles["section-horizontal"]}>
+              {project.title ? <LandingProjectTitle title={project.title} /> : null}
+              <LandingProjectSwiper swiper={project.slides}/>
+              <LandingProjectInfo text={project.info}/>\
+              {project.video ? <LandingProjectVideo video={project.video} /> : null}
+              {project.secondSlider ? <LandingProjectHorizontalSwiper swiper={project.secondSlider} /> : null}
+            </div>
+          ))}
+          <button ref={button} onClick={scrollToTop} className={styles.button}>up</button>
+         </div>
       </section>
     </div>
   );
